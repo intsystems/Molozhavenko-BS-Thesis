c929512282d8a516aee405593d11d2dc7c974ad3
SuperCrabLover <molozhavenko.aa@phystech.edu>
2023-01-18 20:52:26 +0300

    added tests

--- code/BS_First_Part_code/TN_Comp/tests/test_simple.py
+++ code/BS_First_Part_code/TN_Comp/tests/test_simple.py
@@ -1,33 +1,34 @@
-from mylib.train import cv_parameters, Trainer, SyntheticBernuliDataset
-from sklearn.linear_model import LogisticRegression
+import sys
+sys.path.append('../')
 
-def test_sample():
-    assert 0 == 0
+import numpy as np
+import src.mylib.pgm
+from src.mylib.pgm import create_node
+from src.mylib.pgm import create_tensor
+from src.mylib.pgm import create_node_from_pgm
+from src.mylib.pgm import create_image_from_tensor
+from src.mylib.pgm import crop_pgm_image
 
+pgmf_path = "gray8bit_Dataset/"
 
-def test_dataset():
-    dataset = SyntheticBernuliDataset(n=10, m=100, seed=42)
-
-    assert len(dataset.X) == len(dataset.y)
-
-def test_trainer():
-    dataset = SyntheticBernuliDataset(n=10, m=100, seed=42)
-
-    trainer = Trainer(
-        LogisticRegression(penalty='l1', solver='saga', C=1.0),
-        dataset.X, dataset.y,
-    )
-    trainer.train()
+def test_np_eye():
+  I = np.eye(4)
+  output = create_tensor(matrix=I, tensor_base=2, transpose_modes=False)
+  assert output[1] == 4
     
-    assert trainer.eval(output_dict=True)['accuracy'] == 0.96
+def test_inversibility_no_transpose():
+  I = np.random.rand(4, 4)
+  tensor = create_tensor(matrix=I, tensor_base=2, transpose_modes=False)[0]
+  output = create_image_from_tensor(tensor=tensor, transpose_modes=False)
+  assert (output == I).all()
 
-    assert trainer.test(
-        trainer.X_val, trainer.Y_val, output_dict=True
-        )['accuracy'] == 0.96
-
-def test_cv():
-    dataset = SyntheticBernuliDataset(n=10, m=100, seed=42)
-
-    Cs, accuracy, parameters = cv_parameters(dataset.X, dataset.y)
-
-    assert len(Cs) == len(accuracy) == len(parameters)
+def test_inversibility_transpose():
+  I = np.random.rand(4, 4)
+  tensor = create_tensor(matrix=I, tensor_base=2, transpose_modes=True)[0]
+  output = create_image_from_tensor(tensor=tensor, transpose_modes=True)
+  assert (output == I).all()
+    
+def test_create_node():
+  I = np.random.rand(4, 4)
+  output = create_node(matrix=I, tensor_base=2, transpose_modes=False)
+  assert output[1] == 4 and (I == create_image_from_tensor(tensor=output[0].tensor, transpose_modes=False)).all()
